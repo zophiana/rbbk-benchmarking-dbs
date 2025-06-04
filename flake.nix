4ea845b3fa67d071d9616b7f1895092ac68dcc21
zophiana <53372671+zophiana@users.noreply.github.com>
2025-06-04 09:52:31 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          hsqldb
+          netbeans
+          postgresql
+        ];
+      };
+    };
+}
